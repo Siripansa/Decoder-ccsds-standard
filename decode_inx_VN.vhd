f1772090a8ea5f686ff88726a983c5bc957228c0
Siripansa <85486546+Siripansa@users.noreply.github.com>
2021-12-15 12:11:06 +0700

    Add files via upload

---
+++ decode_inx_VN.vhd
@@ -0,0 +1,130 @@
+-- ==============================================================
+-- Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.1 (64-bit)
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- ==============================================================
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+
+entity decode_inx_VN_rom is 
+    generic(
+             DWIDTH     : integer := 5; 
+             AWIDTH     : integer := 5; 
+             MEM_SIZE    : integer := 20
+    ); 
+    port (
+          addr0      : in std_logic_vector(AWIDTH-1 downto 0); 
+          ce0       : in std_logic; 
+          q0         : out std_logic_vector(DWIDTH-1 downto 0);
+          addr1      : in std_logic_vector(AWIDTH-1 downto 0); 
+          ce1       : in std_logic; 
+          q1         : out std_logic_vector(DWIDTH-1 downto 0);
+          clk       : in std_logic
+    ); 
+end entity; 
+
+
+architecture rtl of decode_inx_VN_rom is 
+
+signal addr0_tmp : std_logic_vector(AWIDTH-1 downto 0); 
+signal addr1_tmp : std_logic_vector(AWIDTH-1 downto 0); 
+type mem_array is array (0 to MEM_SIZE-1) of std_logic_vector (DWIDTH-1 downto 0); 
+signal mem : mem_array := (
+    0 => "00001", 1 => "00010", 2 => "01000", 3 => "01010", 4 => "01100", 
+    5 => "00011", 6 => "00100", 7 => "00110", 8 to 9=> "10001", 10 => "00101", 
+    11 => "01001", 12 => "01101", 13 => "01111", 14 => "10001", 15 => "00111", 
+    16 => "01011", 17 => "01110", 18 => "10000", 19 => "10001" );
+
+attribute syn_rom_style : string;
+attribute syn_rom_style of mem : signal is "select_rom";
+attribute ROM_STYLE : string;
+attribute ROM_STYLE of mem : signal is "distributed";
+
+begin 
+
+
+memory_access_guard_0: process (addr0) 
+begin
+      addr0_tmp <= addr0;
+--synthesis translate_off
+      if (CONV_INTEGER(addr0) > mem_size-1) then
+           addr0_tmp <= (others => '0');
+      else 
+           addr0_tmp <= addr0;
+      end if;
+--synthesis translate_on
+end process;
+
+memory_access_guard_1: process (addr1) 
+begin
+      addr1_tmp <= addr1;
+--synthesis translate_off
+      if (CONV_INTEGER(addr1) > mem_size-1) then
+           addr1_tmp <= (others => '0');
+      else 
+           addr1_tmp <= addr1;
+      end if;
+--synthesis translate_on
+end process;
+
+p_rom_access: process (clk)  
+begin 
+    if (clk'event and clk = '1') then
+        if (ce0 = '1') then 
+            q0 <= mem(CONV_INTEGER(addr0_tmp)); 
+        end if;
+        if (ce1 = '1') then 
+            q1 <= mem(CONV_INTEGER(addr1_tmp)); 
+        end if;
+    end if;
+end process;
+
+end rtl;
+
+Library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity decode_inx_VN is
+    generic (
+        DataWidth : INTEGER := 5;
+        AddressRange : INTEGER := 20;
+        AddressWidth : INTEGER := 5);
+    port (
+        reset : IN STD_LOGIC;
+        clk : IN STD_LOGIC;
+        address0 : IN STD_LOGIC_VECTOR(AddressWidth - 1 DOWNTO 0);
+        ce0 : IN STD_LOGIC;
+        q0 : OUT STD_LOGIC_VECTOR(DataWidth - 1 DOWNTO 0);
+        address1 : IN STD_LOGIC_VECTOR(AddressWidth - 1 DOWNTO 0);
+        ce1 : IN STD_LOGIC;
+        q1 : OUT STD_LOGIC_VECTOR(DataWidth - 1 DOWNTO 0));
+end entity;
+
+architecture arch of decode_inx_VN is
+    component decode_inx_VN_rom is
+        port (
+            clk : IN STD_LOGIC;
+            addr0 : IN STD_LOGIC_VECTOR;
+            ce0 : IN STD_LOGIC;
+            q0 : OUT STD_LOGIC_VECTOR;
+            addr1 : IN STD_LOGIC_VECTOR;
+            ce1 : IN STD_LOGIC;
+            q1 : OUT STD_LOGIC_VECTOR);
+    end component;
+
+
+
+begin
+    decode_inx_VN_rom_U :  component decode_inx_VN_rom
+    port map (
+        clk => clk,
+        addr0 => address0,
+        ce0 => ce0,
+        q0 => q0,
+        addr1 => address1,
+        ce1 => ce1,
+        q1 => q1);
+
+end architecture;
+
+
